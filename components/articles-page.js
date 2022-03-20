14f3269694ad35303fef4ab2e8bc791803b7d233
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2022-03-20 13:43:23 -0500

    Add <section> tags for semantics

--- components/articles-page.js
+++ components/articles-page.js
@@ -5,18 +5,23 @@ class ArticlesPage extends LitElement {
         return html`
         <div class="container-fluid">
             <h2>Article Generator</h2>
-            <div class="page-intro">
-                <p>Sometimes I'm in the mood to write (<a href="https://blog.willoughbylabs.com/" target="_blank"
-                        rel="noopener noreferrer">blog</a> | <a href="https://twitter.com/willowbeehive" target="_blank"
-                        rel="noopener noreferrer">Twitter</a>). Shoutout to my favorite writing tool, <a
-                        href="https://www.notion.so/" target="_blank" rel="noopener noreferrer">Notion</a>; it's
-                    pretty
-                    fetch.</p>
-            </div>
-            <div style="width:100%;height:0;padding-bottom:56%;position:relative;">
-                <iframe src="https://giphy.com/embed/3otPoUjeyRisIDxPhK" width="100%" height="100%" style="position:absolute"
-                    frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-            </div> <!-- end iframe -->
+            <section>
+                <div class="page-intro">
+                    <p>Sometimes I'm in the mood to write (
+                        <a href="https://blog.willoughbylabs.com/" target="_blank" rel="noopener noreferrer">blog</a> |
+                        <a href="https://twitter.com/willowbeehive" target="_blank" rel="noopener noreferrer">Twitter</a>).
+                        Shoutout to my favorite writing tool,
+                        <a href="https://www.notion.so/" target="_blank" rel="noopener noreferrer">Notion</a>; it's pretty
+                        fetch.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div style="width:100%;height:0;padding-bottom:56%;position:relative;">
+                    <iframe src="https://giphy.com/embed/3otPoUjeyRisIDxPhK" width="100%" height="100%"
+                        style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                </div> <!-- end iframe -->
+            </section>
         </div> <!-- end container-fluid -->
         `;
     }
--- components/projects-page.js
+++ components/projects-page.js
@@ -8,130 +8,136 @@ class ProjectsPage extends LitElement {
             
             <!-- TODO: make cards into components  -->
             <!-- CScrub -->
-            <div class="card" style="width: 18rem;">
-                <img src="../img/cscrub.png" class="card-img-top"
-                    alt="Views of Terminal windows showing SQL queries to retrieve alderpersons, meetings, legislation, and votes from a database.">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="https://github.com/willoughbylabs/cscrub" target="_blank" rel="noopener noreferrer">CScrub
-                            <i class="bi bi-github"></i>
-                        </a>
-                    </h5>
-                    <p class="card-text">A bot to extract meetings, council members, legislation, and votes from the
-                        Chicago City Clerk's site.</p>
-                    <span class="badge bg-warning text-dark">Feedparser</span>
-                    <span class="badge bg-warning text-dark">PostregreSQL</span>
-                    <span class="badge bg-warning text-dark">Python</span>
-                    <span class="badge bg-warning text-dark">Selenium</span>
-                    <span class="badge bg-warning text-dark">SQLAlchemy</span>
+            <section>
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/cscrub.png" class="card-img-top"
+                        alt="Views of Terminal windows showing SQL queries to retrieve alderpersons, meetings, legislation, and votes from a database.">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="https://github.com/willoughbylabs/cscrub" target="_blank" rel="noopener noreferrer">CScrub
+                                <i class="bi bi-github"></i>
+                            </a>
+                        </h5>
+                        <p class="card-text">A bot to extract meetings, council members, legislation, and votes from the
+                            Chicago City Clerk's site.</p>
+                        <span class="badge bg-warning text-dark">Feedparser</span>
+                        <span class="badge bg-warning text-dark">PostregreSQL</span>
+                        <span class="badge bg-warning text-dark">Python</span>
+                        <span class="badge bg-warning text-dark">Selenium</span>
+                        <span class="badge bg-warning text-dark">SQLAlchemy</span>
+                    </div>
                 </div>
-            </div>
             
-            <!-- CScrub API -->
-            <div class="card" style="width: 18rem;">
-                <img src="../img/cscrub-api.png" class="card-img-top"
-                    alt="An example of returned JSON from a query requesting a city council member and their votes on legislation.">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="https://github.com/willoughbylabs/cscrub-api" target="_blank" rel="noopener noreferrer">CScrub API
-                            <i class="bi bi-github"></i>
-                        </a>
-                    </h5>
+                <!-- CScrub API -->
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/cscrub-api.png" class="card-img-top"
+                        alt="An example of returned JSON from a query requesting a city council member and their votes on legislation.">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="https://github.com/willoughbylabs/cscrub-api" target="_blank" rel="noopener noreferrer">CScrub
+                                API
+                                <i class="bi bi-github"></i>
+                            </a>
+                        </h5>
             
-                    <p class="card-text">A JSON API that shares council members, legislation, and votes from Chicago City Council
-                        meetings.</p>
-                    <span class="badge bg-warning text-dark">API</span>
-                    <span class="badge bg-warning text-dark">Express</span>
-                    <span class="badge bg-warning text-dark">JSON</span>
-                    <span class="badge bg-warning text-dark">Node.js</span>
+                        <p class="card-text">A JSON API that shares council members, legislation, and votes from Chicago City
+                            Council
+                            meetings.</p>
+                        <span class="badge bg-warning text-dark">API</span>
+                        <span class="badge bg-warning text-dark">Express</span>
+                        <span class="badge bg-warning text-dark">JSON</span>
+                        <span class="badge bg-warning text-dark">Node.js</span>
+                    </div>
                 </div>
-            </div>
             
-            <!-- Milton Olive III -->
-            <div class="card" style="width: 18rem;">
-                <img src="../img/milton.png" class="card-img-top" alt="A photo of Milton Lee Olive III in his military uniform.">
-                <div class="card-body">
-                    <h5 class="card-title">
+                <!-- Milton Olive III -->
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/milton.png" class="card-img-top"
+                        alt="A photo of Milton Lee Olive III in his military uniform.">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="https://github.com/willoughbylabs/tribute_Milton-Lee-Olive" target="_blank"
+                                rel="noopener noreferrer">Milton Lee Olive III Tribute
+                            </a>
+                        </h5>
                         <a href="https://github.com/willoughbylabs/tribute_Milton-Lee-Olive" target="_blank"
-                            rel="noopener noreferrer">Milton Lee Olive III Tribute
+                            rel="noopener noreferrer">
+                            <i class="bi bi-github"></i>
                         </a>
-                    </h5>
-                    <a href="https://github.com/willoughbylabs/tribute_Milton-Lee-Olive" target="_blank" rel="noopener noreferrer">
-                        <i class="bi bi-github"></i>
-                    </a>
             
-                    <p class="card-text">A tribute to the Black, Medal of Honor veteran from Chicago who sacrificed his life for
-                        others.</p>
-                    <span class="badge bg-warning text-dark">Bootstrap 3</span>
-                    <span class="badge bg-warning text-dark">CSS</span>
-                    <span class="badge bg-warning text-dark">GitHub Pages</span>
-                    <span class="badge bg-warning text-dark">HTML</span>
-                    <span class="badge bg-warning text-dark">JavaScript</span>
+                        <p class="card-text">A tribute to the Black, Medal of Honor veteran from Chicago who sacrificed his life for
+                            others.</p>
+                        <span class="badge bg-warning text-dark">Bootstrap 3</span>
+                        <span class="badge bg-warning text-dark">CSS</span>
+                        <span class="badge bg-warning text-dark">GitHub Pages</span>
+                        <span class="badge bg-warning text-dark">HTML</span>
+                        <span class="badge bg-warning text-dark">JavaScript</span>
+                    </div>
                 </div>
-            </div>
             
-            <!-- myChi -->
-            <div class="card" style="width: 18rem;">
-                <img src="../img/mychi.png" class="card-img-top"
-                    alt="A dashboard with cards showing predictions for train arrivals from the Chicago Transit Authority.">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="http://mychi.willoughbylabs.com/" target="_blank" rel="noopener noreferrer">myChi
+                <!-- myChi -->
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/mychi.png" class="card-img-top"
+                        alt="A dashboard with cards showing predictions for train arrivals from the Chicago Transit Authority.">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="http://mychi.willoughbylabs.com/" target="_blank" rel="noopener noreferrer">myChi
+                            </a>
+                        </h5>
+                        <a href="https://github.com/willoughbylabs/mychi" target="_blank" rel="noopener noreferrer">
+                            <i class="bi bi-github"></i>
                         </a>
-                    </h5>
-                    <a href="https://github.com/willoughbylabs/mychi" target="_blank" rel="noopener noreferrer">
-                        <i class="bi bi-github"></i>
-                    </a>
-                    <p class="card-text">A template for a dashboard to display Chicago open data in an at-a-glance, concise
-                        format. Built with the Train Tracker API.</p>
-                    <span class="badge bg-warning text-dark">API</span>
-                    <span class="badge bg-warning text-dark">Bootstrap 5</span>
-                    <span class="badge bg-warning text-dark">CSS</span>
-                    <span class="badge bg-warning text-dark">Flask-SQLALchemy</span>
-                    <span class="badge bg-warning text-dark">Heroku</span>
-                    <span class="badge bg-warning text-dark">HTML</span>
-                    <span class="badge bg-warning text-dark">Jasmine</span>
-                    <span class="badge bg-warning text-dark">JSON</span>
-                    <span class="badge bg-warning text-dark">JavaScript</span>
-                    <span class="badge bg-warning text-dark">Python</span>
+                        <p class="card-text">A template for a dashboard to display Chicago open data in an at-a-glance, concise
+                            format. Built with the Train Tracker API.</p>
+                        <span class="badge bg-warning text-dark">API</span>
+                        <span class="badge bg-warning text-dark">Bootstrap 5</span>
+                        <span class="badge bg-warning text-dark">CSS</span>
+                        <span class="badge bg-warning text-dark">Flask-SQLALchemy</span>
+                        <span class="badge bg-warning text-dark">Heroku</span>
+                        <span class="badge bg-warning text-dark">HTML</span>
+                        <span class="badge bg-warning text-dark">Jasmine</span>
+                        <span class="badge bg-warning text-dark">JSON</span>
+                        <span class="badge bg-warning text-dark">JavaScript</span>
+                        <span class="badge bg-warning text-dark">Python</span>
+                    </div>
                 </div>
-            </div>
             
-            <!-- Photography -->
-            <div class="card" style="width: 18rem;">
-                <img src="../img/tribune-thumb.jpg" class="card-img-top"
-                    alt="Three people looking at the gothic architecture of Chicago's Tribune Tower from a large balcony.">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="https://unsplash.com/@willoughbylabs" target="_blank" rel="noopener noreferrer">Photography
-                        </a>
-                    </h5>
-                    <p class="card-text">Snapshots from here and there.</p>
-                    <span class="badge bg-warning text-dark">Pixel 6 Pro</span>
-                    <span class="badge bg-warning text-dark">Pixel 3a XL</span>
-                    <span class="badge bg-warning text-dark">Samsung Galaxy S II</span>
+                <!-- Photography -->
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/tribune-thumb.jpg" class="card-img-top"
+                        alt="Three people looking at the gothic architecture of Chicago's Tribune Tower from a large balcony.">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="https://unsplash.com/@willoughbylabs" target="_blank" rel="noopener noreferrer">Photography
+                            </a>
+                        </h5>
+                        <p class="card-text">Snapshots from here and there.</p>
+                        <span class="badge bg-warning text-dark">Pixel 6 Pro</span>
+                        <span class="badge bg-warning text-dark">Pixel 3a XL</span>
+                        <span class="badge bg-warning text-dark">Samsung Galaxy S II</span>
+                    </div>
                 </div>
-            </div>
             
-            <!-- Willoughby Labs -->
-            <div class="card" style="width: 18rem;">
-                <img src="../img/wlabs.png" class="card-img-top"
-                    alt="View of Chicago's Tribune Tower and southern skyline, as seen from the balcony of an apartment building's common-area balcony.">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="http://www.willoughbylabs.com" target="_blank" rel="noopener noreferrer">Willoughby Labs
-                        </a>
-                    </h5>
-                    <a href="https://github.com/willoughbylabs/willoughby-labs" target="_blank" rel="noopener noreferrer"><i
-                            class="bi bi-github"></i></a>
-                    <p class="card-text">Personal site and playground for learning.</p>
-                    <span class="badge bg-warning text-dark">Bootstrap 5</span>
-                    <span class="badge bg-warning text-dark">CSS</span>
-                    <span class="badge bg-warning text-dark">HTML</span>
-                    <span class="badge bg-warning text-dark">JavaScript</span>
-                    <span class="badge bg-warning text-dark">LitElement</span>
+                <!-- Willoughby Labs -->
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/wlabs.png" class="card-img-top"
+                        alt="View of Chicago's Tribune Tower and southern skyline, as seen from the balcony of an apartment building's common-area balcony.">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="http://www.willoughbylabs.com" target="_blank" rel="noopener noreferrer">Willoughby Labs
+                            </a>
+                        </h5>
+                        <a href="https://github.com/willoughbylabs/willoughby-labs" target="_blank" rel="noopener noreferrer"><i
+                                class="bi bi-github"></i></a>
+                        <p class="card-text">Personal site and playground for learning.</p>
+                        <span class="badge bg-warning text-dark">Bootstrap 5</span>
+                        <span class="badge bg-warning text-dark">CSS</span>
+                        <span class="badge bg-warning text-dark">HTML</span>
+                        <span class="badge bg-warning text-dark">JavaScript</span>
+                        <span class="badge bg-warning text-dark">LitElement</span>
+                    </div>
                 </div>
-            </div>
+            </section>
         `;
     }
     createRenderRoot() {
